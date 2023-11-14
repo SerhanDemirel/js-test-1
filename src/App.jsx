7040b95e430401e4401840b6246d795ba638c367
Serhan Demirel <102233776+SerhanDemirel@users.noreply.github.com>
2023-11-14 18:03:43 +0300

    js-test-1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TicTacToe from "./components/TicTacToe/TicTacToe";
+
+const App = () => {
+  return (
+    <div>
+      <TicTacToe></TicTacToe>
+    </div>
+  );
+};
+
+export default App;
